ffe655728b2ecf2ce6d6841f0df9111e44704e89
Li Fang Wang <79581783+thebest513@users.noreply.github.com>
2022-12-06 10:19:38 +0800

    Update del_folder.sh

--- del_folder.sh
+++ del_folder.sh
@@ -6,5 +6,5 @@ for folder in `cat folder_del.txt | tr "\r" " "`
 do
 # Redirecting both std output and std error, the former file size is smaller than the latter
 # csh does not allow redirecting std error only
-ctm deploy folder::delete CTM $folder >& $folder.json
+ctm deploy folder::delete CTM $folder 1> /dev/null 2> $folder.json
 done
